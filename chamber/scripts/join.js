9be34f073e8f216732962438bf764be4b3a1df32
Shema <shemalucas20k@gmail.com>
2026-02-06 09:39:57 +0200

    corrected some modals and membership input to meet criteria

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const year = document.getElementById("year");
-  if (year) {
-    year.textContent = new Date().getFullYear();
-  }
-
-  const lastModified = document.getElementById("lastModified");
-  if (lastModified) {
-    lastModified.textContent = document.lastModified;
-  }
+  if (year) year.textContent = new Date().getFullYear();
 
   const timestamp = document.getElementById("timestamp");
-  if (timestamp) {
-    timestamp.value = new Date().toISOString();
-  }
+  if (timestamp) timestamp.value = new Date().toISOString();
+
+  document.querySelectorAll("[data-modal]").forEach(button => {
+    const modal = document.getElementById(button.dataset.modal);
+    const close = modal.querySelector("button");
+
+    button.addEventListener("click", () => modal.showModal());
+    close.addEventListener("click", () => modal.close());
+  });
 });
